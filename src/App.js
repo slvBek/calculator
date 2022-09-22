0ee680b23b1ff70b0218c79d9d4fa2842ec19661
Student astrum <student@astrum.domain>
2022-09-22 18:29:45 +0500

    style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, {useState} from "react";
 import "./App.css";
+import "./component/style.css"
+import $ from 'jquery';
 
 const App = () => {
   const [input, setInput] = useState("");
@@ -42,44 +44,40 @@ const App = () => {
     
   );
 };
-const colors = ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36"];
-
-const numBalls = 50;
-const balls = [];
-
-for (let i = 0; i < numBalls; i++) {
-  let ball = document.createElement("div");
-  ball.classList.add("ball");
-  ball.style.background = colors[Math.floor(Math.random() * colors.length)];
-  ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
-  ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
-  ball.style.transform = `scale(${Math.random()})`;
-  ball.style.width = `${Math.random()}em`;
-  ball.style.height = ball.style.width;
-  
-  balls.push(ball);
-  document.body.append(ball);
-}
-balls.forEach((el, i) => {
-  let to = {
-    x: Math.random() * (i % 2 === 0 ? -11 : 11),
-    y: Math.random() * 12
-  };
-
-  el.animate(
-    [
-      { transform: "translate(0, 0)" },
-      { transform: `translate(${to.x}rem, ${to.y}rem)` }
-    ],
-    {
-      duration: (Math.random() + 1) * 2000,
-      direction: "alternate",
-      fill: "both",
-      iterations: Infinity,
-      easing: "ease-in-out"
-    }
-  );
+$(document).ready(function() {
+  setAnimations();
 });
+
+function setAnimations() {
+  var collection = $(".fadeOut");
+  collection.each(function() {
+    animateNumber($(this));
+  });
+}
+
+function animateNumber(x) {
+  var newPosition = makeNewPosition();
+  var newFontSize = makeNewFontSize();
+  x.css({ top: newPosition[0], left: newPosition[1], fontSize: newFontSize });
+  x.animate({ fontSize: newFontSize + 20}, 5000, function() {
+    animateNumber(x);
+  }); 
+}
+
+function makeNewPosition() {
+  var h = $(window).height() - 50;
+  var w = $(window).width() - 50;
+
+  var nh = Math.floor(Math.random() * h);
+  var nw = Math.floor(Math.random() * w);
+
+  return [nh, nw];
+}
+
+function makeNewFontSize() {
+  return Math.floor(Math.random() * 18);
+}
+
 
 
 export default App;
